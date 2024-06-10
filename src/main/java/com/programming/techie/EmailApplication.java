dd011be415debefbf19693a1fd15a6fbe68642a6
AdhithyanM <adhithyanmanoharan26@gmail.com>
2024-06-11 00:50:51 +0530

    added spring dependencies

--- src/main/java/com/programming/techie/EmailApplication.java
+++ src/main/java/com/programming/techie/EmailApplication.java
@@ -2,7 +2,7 @@ package com.programming.techie;
 
 public class EmailApplication {
     public static void main(String[] args) {
-        EmailClient emailClient = new EmailClient(new BasicSpellChecker());
+        EmailClient emailClient = new EmailClient(new AdvancedSpellChecker());
         emailClient.sendEmail("Hey, " +
                 "This is my first email message.");
         emailClient.sendEmail("Hey, " +
